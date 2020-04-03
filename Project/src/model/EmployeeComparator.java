575d44b5ffbac0f2ebd49ca600d6a9ea9fd3b85e
AnaOlteanu <49120666+AnaOlteanu@users.noreply.github.com>
2020-04-03 18:16:56 +0300

    Delete EmployeeComparator.java

--- Project/src/model/EmployeeComparator.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-import java.util.Comparator;
-
-public class EmployeeComparator implements Comparator<Employee> {
-    public int compare(Employee e1, Employee e2) {
-        return e1.getNume().compareTo(e2.getNume());
-    }
-}
